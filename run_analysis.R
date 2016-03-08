1277dc964ee5d4469f41a8d4a1dd329a6ad13853
jrhouse911 <jhouse4@gmail.com>
2016-03-08 07:47:39 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,69 +1,49 @@
-###run_analysis.R
-if (!require("data.table")) {
-        install.packages("data.table")
-}
+setwd("~/GitHub/datasciencecoursera/GetCelanData")
 
-if (!require("reshape2")) {
-        install.packages("reshape2")
-}
+#reading activity and features list
+activity<-read.table("activity_labels.txt",sep = "",col.names =c("Activity","ActivityDesc"))
+features<-read.table("features.txt")
 
-require("data.table")
-require("reshape2")
+#Changing variable names
+features_names<-features[,2]
+f_vector<- as.vector(features_names)
+featurenames<-gsub("[\\(\\)]","",f_vector)
+featurenames<-gsub("-",".",featurenames)
+featurenames<-gsub(",",".",featurenames)
+featurenames<-gsub("fbodybody","fbody",featurenames)
+featurenames<-tolower(featurenames)
 
-# Load: activity labels
-activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
+#reading training data
+subject_train<-read.table("subject_train.txt",sep = "", col.names = "subject_code")
+x_train<-read.table("X_train.txt" ,sep = "")
+y_train<-read.table("y_train.txt",sep = "", col.names = "activity")
+xtg<-cbind(y_train,subject_train,x_train)
+names(xtg)<-c("Activity","Performer",featurenames)
 
-# Load: data column names
-features <- read.table("./UCI HAR Dataset/features.txt")[,2]
+#reading test data
+subject_test<-read.table("subject_test.txt",sep = "", col.names = "subject_code")
+x_test<-read.table("X_test.txt" ,sep = "")
+y_test<-read.table("y_test.txt",sep = "", col.names = "activity")
+xtr<-cbind(y_test,subject_test,x_test)
+names(xtr)<-c("Activity","Performer",featurenames)
 
-# Extract: only the measurements on the mean and standard deviation for each measurement
-extract_features <- grepl("mean|std", features)
+#combined test and training data
+combineddata<-rbind(xtg,xtr)
 
-# Load & Process: x_test & y_test data
-x_test <- read.table("./UCI HAR Dataset/test/x_test.txt")
-y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
-subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+#selecting only the mean and std columns
+newdata<-combineddata[,grepl("Activity|Performer|[\\(.)]mean|std", colnames(combineddata))]
 
-names(x_test) = features
 
-# Extract: only the measurements on the mean and standard deviation for each measurement
-x_test = x_test[,extract_features]
+library(dplyr)
+newdata<-as.data.frame(newdata)
+#removed mean frequency column selected because of grepl of mean
+newdata2<-select(newdata,-contains("meanfreq"))
 
-# Load: activity labels
-y_test[,2] = activity_labels[y_test[,1]]
-names(y_test) = c("Activity_ID", "Activity_Label")
-names(subject_test) = "subject"
+#Join activity table to get the activity description
+finaldata <-inner_join(newdata2,activity,by ="Activity")
 
-# Bind data
-test_data <- cbind(as.data.table(subject_test), y_test, x_test)
-
-# Load & Process: x_train & y_train data
-x_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
-y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
-
-subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
-
-names(x_train) = features
-
-# Extract: only the measurements on the mean and standard deviation for each measurement
-x_train = x_train[,extract_features]
-
-# Load: activity data
-y_train[,2] = activity_labels[y_train[,1]]
-names(y_train) = c("Activity_ID", "Activity_Label")
-names(subject_train) = "subject"
-
-# Bind data
-train_data <- cbind(as.data.table(subject_train), y_train, x_train)
-
-# Merge test and train data
-data = rbind(test_data, train_data)
-
-id_labels   = c("subject", "Activity_ID", "Activity_Label")
-data_labels = setdiff(colnames(data), id_labels)
-melt_data      = melt(data, id = id_labels, measure.vars = data_labels)
-
-# Apply mean function to dataset using dcast function
-tidy_data   = dcast(melt_data, subject + Activity_Label ~ variable, mean)
-
-write.table(tidy_data, "./tidy_data.txt", row.names=FALSE)
+#Group by and summary to the new file.
+finaldata<-select(finaldata,-Activity)
+by_activitysubject<-group_by(finaldata,ActivityDesc,Performer)
+finaldataset<-summarise_each(by_activitysubject,funs(mean))
+write.table(finaldataset,"summary.txt",row.names = FALSE)
